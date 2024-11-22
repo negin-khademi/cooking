b71fb0e4863f82e986089d80e20c5070a9c1e236
Negin <negin.khademi@gmail.com>
2024-11-22 21:22:44 +0330

    recipe list component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,15 @@
 import './Home.css';
 
+import RecipeList from '../../components/RecipeList';
+import { useFetch } from '../../hooks/useFetch';
+
 export default function Home() {
+  const { data, isPending, error } = useFetch('http://localhost:3000/recipes');
   return (
-    <div>
-      <p>home component</p>
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p>Loddind ...</p>}
+      {data && <RecipeList recipes={data} />}
     </div>
   );
 }
